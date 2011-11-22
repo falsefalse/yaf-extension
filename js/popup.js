726dc16b4facbce936a84500e1dba745a6d2c3aa
Ilya Furman <smashlong@gmail.com>
2011-11-22 13:15:12 +0200

    Use templates for views, creating stuff manually sucks

--- js/popup.js
+++ js/popup.js
@@ -5,74 +5,37 @@
 /*global browser:true, chrome:true, YAF:true, _gaq:true*/
 
 YAF = {
-    service : chrome.extension.getBackgroundPage(),
-    services : {
-        'whois' : {
-            url : "http://whois.domaintools.com/%d",
-            label : 'WhoIs'
-        }
-    },
-    createElement : function(tag, html, className) {
-        var element = document.createElement(tag);
-        element.innerHTML = html;
-        element.className = className;
-        return element;
-    }
+    service : chrome.extension.getBackgroundPage()
 };
 
 window.addEventListener("DOMContentLoaded", function() {
     chrome.tabs.getSelected(null, function(tab) {
         var data = YAF.service.YAF.tabs[tab.id],
             geo  = data.geo,
-            domain = data.domain
+            domain = data.domain;
 
         YAF.service._gaq.push(['_trackPageview']);
 
         var ul = document.querySelector('#menu');
 
         if (!geo) {
-            ul.appendChild(YAF.createElement('li', domain, 'data'));
-            ul.appendChild(YAF.createElement('li', 'Was not found in database', 'data small'));
+            ul.innerHTML = window.tmpl('not_found', {
+                domain: domain
+            });
             return;
         }
 
         if (geo.isLocal) {
-            ul.appendChild(YAF.createElement('li', 'Local resource', 'data'));
-            ul.appendChild(YAF.createElement('li', domain, 'data small'));
+            ul.innerHTML = window.tmpl('local', {
+                domain: domain,
+                geo: geo
+            });
             return;
         }
 
-        ul.appendChild(YAF.createElement('li', geo.country_name, 'data capitalize'));
-
-        var region = [];
-        if (geo.city) region.push(geo.city);
-        if (geo.region && geo.region != geo.city) region.push(geo.region);
-        if (region.length) ul.appendChild(YAF.createElement('li', region.join(', '), 'data small capitalize'));
-
-        ul.appendChild(YAF.createElement('li', geo.ip, 'data small'));
-
-        ul.appendChild(YAF.createElement('li', '', 'separator'));
-
-        for (var name in YAF.services) {
-            var url = YAF.services[name].url
-                        .replace(/\%d/, data.domain)
-                        .replace(/\%i/, geo.ip);
-
-            var link = YAF.createElement('a', YAF.services[name].label, 'service ' + name);
-
-            link.addEventListener('click', (function(url) {
-                return function(event) {
-                    chrome.tabs.create({
-                        url      : url,
-                        selected : true
-                    });
-                    _gaq.push(['_trackEvent', 'link-whois', 'clicked']);
-                };
-            })(url), false);
-
-            var li = document.createElement('li');
-            li.appendChild(link);
-            ul.appendChild(li);
-        }
+        ul.innerHTML = window.tmpl('regular', {
+            domain: domain,
+            geo: geo
+        });
     });
 }, false);
