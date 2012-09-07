820d83fb2a6971eadffd506b524d44df7a001651
Ilya Furman <smashlong@gmail.com>
2012-09-07 18:12:19 +0300

    Finally make GA work

--- js/service.js
+++ js/service.js
@@ -5,6 +5,13 @@
 
 window._gaq = window._gaq || [];
 _gaq.push(['_setAccount', 'UA-18454737-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 YAF = {
     API : {
